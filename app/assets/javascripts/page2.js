afdf45c908cefb4ca7aec35da888261c11bc7132
jason thein <emailmejason@yahoo.com>
2013-07-20 11:20:08 -0700

    drag and drop to specific container, added bracketcat, push to array upon correct container

--- app/assets/javascripts/page2.js
+++ app/assets/javascripts/page2.js
@@ -8,28 +8,38 @@ $(document).ready(function(){
 
 
 
-  $( "#draggable" ).draggable();
-  $( ".droppable" ).droppable({
-    drop: function() {
-      alert( "dropped" );
-    }
-  });
 
+
+// ________________
+    var correct =[]
   $("div.droppable").droppable({
       accept: function (elm) {
           var $this = $(this);
-          if ($this.data("question-id") == elm.data("question-id"))
-              return true;
-          return false;
+          if ($this.data("question-id") == elm.data("question-id")){
+              ;
+              return true ;
+
+          }return false;
       },
       drop: function(e,ui) {
           console.log(e, ui);
-      }
-  });
+          alert("yes");
+          correct.push(1);
+          alert(correct)
+     if (correct.length==2){
+         alert("congratulations")
+       } }
 
+  });
+  if (correct.length==2){
+      alert("congratulations")
+    }
   $("div.draggable").draggable({
       revert: "invalid"
   });
+
+
+
 });
 
 
