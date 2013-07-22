7370ee0a22bb32ccd062f5d525350a6f03b746ac
jason thein <emailmejason@yahoo.com>
2013-07-21 18:35:32 -0700

    bubble text working as well as link to form

--- app/assets/javascripts/page2.js
+++ app/assets/javascripts/page2.js
@@ -3,16 +3,19 @@ $(document).ready(function(){
   var showkey = function(){
     $("#key").removeClass("inv_img");
     $("#text_key").removeClass("inv");
+    $(".onkey").removeClass("inv");
     // $( "#text_key" ).show( "bounce", 1000 );
   };
 
-  $("#cat").on("click", showkey);
+  $(".catbox").on("click", showkey);
 
 
   var showtag = function(){
     $("#tag").removeClass("inv_img");
     $("#text_key").addClass("inv");
     $("#text_tag").removeClass("inv");
+    $(".ontag").removeClass("inv");
+    $(".onkey").addClass("inv");
   };
 
   $("#key").on("click", showtag);
@@ -21,7 +24,8 @@ $(document).ready(function(){
     $("#rocket").removeClass("inv_img");
     $("#text_tag").addClass("inv");
     $("#text_rocket").removeClass("inv");
-
+    $(".onrocket").removeClass("inv");
+    $(".ontag").addClass("inv");
   };
 
   $("#tag").on("click", showrocket);
@@ -31,7 +35,8 @@ $(document).ready(function(){
     $(".bracket").removeClass("inv_img");
     $("#text_rocket").addClass("inv");
     $("#text_bracket").removeClass("inv");
-
+    $(".onbracket").removeClass("inv");
+    $(".onrocket").addClass("inv");
   };
 
   $("#rocket").on("click", showbracket);
